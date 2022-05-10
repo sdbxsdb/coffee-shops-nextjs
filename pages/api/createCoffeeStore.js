93ba859be2f8ff9218c486069ae15860d1fa731e
Sam.Donaghy-Bell.76008920 <Sam.Donaghy-Bell@microfocus.com>
2022-05-10 12:10:56 +0100

    working refactor before using swr

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,25 +1,25 @@
-import { table, getMinifiedRecords } from "../../lib/airtable";
+import {
+  table,
+  getMinifiedRecords,
+  findRecordByFilter,
+} from "../../lib/airtable";
 
 const createCoffeeStore = async (req, res) => {
   if (req.method === "POST") {
     //find a record
-    const { id, name, address, neighborhood, imgUrl, voting } = req.body;
+
+    const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
+
 
     try {
       if (id) {
-        // console.log('req body', req.body);
 
-        const findCoffeeStoreRecords = await table
-          .select({
-            filterByFormula: `id="${id}"`,
-          })
-          .firstPage();
+        const records = await findRecordByFilter(id);
 
-        if (findCoffeeStoreRecords.length !== 0) {
-          const records = getMinifiedRecords(findCoffeeStoreRecords);
+        if (records.length !== 0) {
           res.json(records);
         } else {
-          //create a new record
+          //create a record
           if (name) {
             const createRecords = await table.create([
               {
@@ -27,38 +27,28 @@ const createCoffeeStore = async (req, res) => {
                   id,
                   name,
                   address,
-                  neighborhood,
-                  imgUrl,
+                  neighbourhood,
                   voting,
+                  imgUrl,
                 },
               },
             ]);
 
             const records = getMinifiedRecords(createRecords);
-
-            res.json({
-              records,
-            });
+            res.json(records);
           } else {
             res.status(400);
-            res.json({
-              message: "id or name is missing",
-            });
+            res.json({ message: "Id or name is missing" });
           }
         }
       } else {
         res.status(400);
-        res.json({
-          message: "id is missing",
-        });
+        res.json({ message: "Id is missing" });
       }
     } catch (err) {
-      console.error("Error creating or finding store", err);
+      console.error("Error creating or finding a store", err);
       res.status(500);
-      res.json({
-        message: "Error creating or finding store",
-        err,
-      });
+      res.json({ message: "Error creating or finding a store", err });
     }
   }
 };
